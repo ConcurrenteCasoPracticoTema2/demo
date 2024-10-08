9f9ea8630f6d2f6a5918c9a2c24b20f7c51a9c71
jmartter <150058251+jmartter@users.noreply.github.com>
2024-10-08 17:23:12 +0200

    .

--- src/main/java/com/example/demo/hilo/AsyncService.java
+++ src/main/java/com/example/demo/hilo/AsyncService.java
@@ -1,23 +1,35 @@
-
 package com.example.demo.hilo;
+
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 @Service
 public class AsyncService {
+
+    private final Semaphore semaphore = new Semaphore(1); // Permite a 5 hilos acceder a la vez
 
     @Async("taskExecutor")
     public void printData() {
         try {
             CSVReaderService csvReaderService = new CSVReaderService("src/main/resources/IQ_level_limpio.csv");
-            ExecutorService executor = Executors.newFixedThreadPool(5);
+            ExecutorService executor = Executors.newFixedThreadPool(30);
 
-            for (int i = 0; i < 10; i++) {
-                executor.submit(new CSVProcessingTask(csvReaderService));
+            for (int i = 0; i < 109; i++) {
+                executor.submit(() -> {
+                    try {
+                        semaphore.acquire(); // Adquirir un permiso antes de ejecutar la tarea
+                        new CSVProcessingTask(csvReaderService).run();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    } finally {
+                        semaphore.release(); // Liberar el permiso después de ejecutar la tarea
+                    }
+                });
             }
 
             executor.shutdown();
